257784e6d719a2b0186bc7d856f617ae821ae9a3
Gowrysankar <138871371+Gowrysankar@users.noreply.github.com>
2023-07-14 14:17:02 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,19 +4,19 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "29802408"))
-	API_HASH = os.environ.get("API_HASH", "a6f4235751213eb700d6ef19be1a4135"))
-	BOT_TOKEN = os.environ.get("BOT_TOKEN", "6363338869:AAHBdQvk5u62fP7mUUdWYO9zG3QCYibC4xg"))
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", "ClawCinemasFileStoreBot"))
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001882949288"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "6291642176"))
-	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://achyuth:achyuth@cluster0.mu8z6rp.mongodb.net/?retryWrites=true&w=majority"))
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001882949288")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001882949288"))
-	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
+	API_ID = int(os.environ.get("API_ID", "0"))
+	API_HASH = os.environ.get("API_HASH")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
+	DATABASE_URL = os.environ.get("DATABASE_URL")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
+	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
+	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot. 
